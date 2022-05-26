514760edc1b2ba1c7dde25b981867f497979047c
Adam Roderick <aroder@gmail.com>
2022-05-26 16:08:50 -0600

    pre-commit configuration

--- target_gcs/sinks.py
+++ target_gcs/sinks.py
@@ -19,28 +19,44 @@ class GCSSink(RecordSink):
     max_size = 1000  # Max records to write in one batch
 
     def __init__(self, target, stream_name, schema, key_properties):
-        super().__init__(target=target, stream_name=stream_name, schema=schema, key_properties=key_properties)
+        super().__init__(
+            target=target,
+            stream_name=stream_name,
+            schema=schema,
+            key_properties=key_properties,
+        )
         self._gcs_write_handle = None
         self.key_name = self.build_key_name(self.config)
 
-
     def build_key_name(self, config):
         extraction_timestamp = round(time.time())
-        key_name = self.config.get("key_naming_convention", f"{self.stream_name}_{extraction_timestamp}.{self.output_format}")
-        key_name = f'{self.config.get("key_prefix", "")}/{key_name}'.replace('//', '/')
-        if key_name.startswith('/'):
+        key_name = self.config.get(
+            "key_naming_convention",
+            f"{self.stream_name}_{extraction_timestamp}.{self.output_format}",
+        )
+        key_name = f'{self.config.get("key_prefix", "")}/{key_name}'.replace("//", "/")
+        if key_name.startswith("/"):
             key_name = key_name[1:]
-        date = datetime.today().strftime(self.config.get('date_format', '%Y-%m-%d'))
-        key_name = key_name.format_map(defaultdict(str, stream=self.stream_name, date=date, timestamp=extraction_timestamp))
+        date = datetime.today().strftime(self.config.get("date_format", "%Y-%m-%d"))
+        key_name = key_name.format_map(
+            defaultdict(
+                str, stream=self.stream_name, date=date, timestamp=extraction_timestamp
+            )
+        )
         return key_name
-
 
     @property
     def gcs_write_handle(self):
         """opens a stream for writing to the target cloud object"""
         if not self._gcs_write_handle:
             credentials_path = self.config.get("credentials_file")
-            self._gcs_write_handle = open(f'gs://{self.config.get("bucket_name")}/{self.key_name}', 'wb', transport_params=dict(client=Client.from_service_account_json(credentials_path)))
+            self._gcs_write_handle = open(
+                f'gs://{self.config.get("bucket_name")}/{self.key_name}',
+                "wb",
+                transport_params=dict(
+                    client=Client.from_service_account_json(credentials_path)
+                ),
+            )
         return self._gcs_write_handle
 
     @property
@@ -54,4 +70,6 @@ class GCSSink(RecordSink):
         Developers may optionally read or write additional markers within the
         passed `context` dict from the current batch.
         """
-        self.gcs_write_handle.write(f"{json.dumps(record, default=str)}\n".encode('utf-8'))
+        self.gcs_write_handle.write(
+            f"{json.dumps(record, default=str)}\n".encode("utf-8")
+        )
--- target_gcs/tests/test_sinks.py
+++ target_gcs/tests/test_sinks.py
@@ -1,51 +1,59 @@
 from datetime import datetime
-import re 
+import re
 
 from target_gcs.sinks import GCSSink
 from target_gcs.target import TargetGCS
 
+
 def build_sink(config={}):
-    default_config = {
-        'bucket_name': 'test-bucket',
-        'credentials_file': './creds.json'
-    }
+    default_config = {"bucket_name": "test-bucket", "credentials_file": "./creds.json"}
     config.update(default_config)
     print(232222, config)
-    return GCSSink(TargetGCS(config=config), 'my_stream', {'properties': {}}, key_properties=config)
-    
+    return GCSSink(
+        TargetGCS(config=config), "my_stream", {"properties": {}}, key_properties=config
+    )
+
 
 def test_extraction_timestamp_is_unix_time():
     subject = build_sink()
-    assert re.match(r'my_stream_\d+.jsonl', subject.key_name)
-    
+    assert re.match(r"my_stream_\d+.jsonl", subject.key_name)
+
 
 def test_key_name_includes_prefix_when_provided():
-    subject = build_sink({'key_prefix': 'asdf'})
-    assert re.match(r'asdf/my_stream', subject.key_name)
+    subject = build_sink({"key_prefix": "asdf"})
+    assert re.match(r"asdf/my_stream", subject.key_name)
+
 
 def test_key_name_does_not_start_with_slash():
-    subject = build_sink({'key_prefix': '/asdf'})
-    assert not subject.key_name.startswith('/')
+    subject = build_sink({"key_prefix": "/asdf"})
+    assert not subject.key_name.startswith("/")
+
 
 def test_key_name_includes_stream_name_when_naming_convention_not_provided():
-    subject = build_sink({'key_naming_convention': 'asdf.txt'})
-    assert 'asdf.txt' == subject.key_name
+    subject = build_sink({"key_naming_convention": "asdf.txt"})
+    assert "asdf.txt" == subject.key_name
+
 
 def test_key_name_includes_stream_name_if_stream_token_used():
-    subject = build_sink({'key_naming_convention': '___{stream}___.txt'})
-    assert '___my_stream___.txt' == subject.key_name
+    subject = build_sink({"key_naming_convention": "___{stream}___.txt"})
+    assert "___my_stream___.txt" == subject.key_name
+
 
 def test_key_name_includes_default_date_format_if_date_token_used():
-    date_format = '%Y-%m-%d'
-    subject = build_sink({'key_naming_convention': 'file/{date}.txt'})
-    assert f'file/{datetime.today().strftime(date_format)}.txt' == subject.key_name
+    date_format = "%Y-%m-%d"
+    subject = build_sink({"key_naming_convention": "file/{date}.txt"})
+    assert f"file/{datetime.today().strftime(date_format)}.txt" == subject.key_name
+
 
 def test_key_name_includes_date_format_if_date_token_used_and_date_format_provided():
-    date_format = '%m %d, %Y'
-    subject = build_sink({'key_naming_convention': 'file/{date}.txt', 'date_format': date_format})
-    assert f'file/{datetime.today().strftime(date_format)}.txt' == subject.key_name
+    date_format = "%m %d, %Y"
+    subject = build_sink(
+        {"key_naming_convention": "file/{date}.txt", "date_format": date_format}
+    )
+    assert f"file/{datetime.today().strftime(date_format)}.txt" == subject.key_name
+
 
 def test_key_name_includes_timestamp_if_timestamp_token_used():
-    subject = build_sink({'key_naming_convention': 'file/{timestamp}.txt'})
+    subject = build_sink({"key_naming_convention": "file/{timestamp}.txt"})
     print(subject.key_name)
-    assert re.match(f'file/\d+.txt', subject.key_name)
+    assert re.match(r"file/\d+.txt", subject.key_name)
